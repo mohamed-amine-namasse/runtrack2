9fecbef1839b44340110903b30d54cc8d8f642f7
MedAmine36 <mohamed-amine.namasse@laplateforme.io>
2025-07-21 11:52:18 +0200

    editjob1

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -10,9 +10,33 @@
 <body>
     <?php 
     
-    $str=
+    $str="LaPlaterforme";
+    echo $str;
+    echo "<br>";
+    $str2="Vive ";
+    $str3=" !";
+    $concat=$str2.$str.$str3;
+    echo $concat;
 
+    echo "<br>";
+    $val=6;
+    echo "\$val:";
+    echo $val;
+    echo "<br>";
+    $val=$val+4;
+    echo "\$val:";
+    echo $val;
+
+    echo "<br>";
+    $myBool=true;
+    echo "\$myBool:";
+    echo $myBool;
+    echo "<br>";
+    $myBool=false;
+    echo "\$myBool:";
+    echo $myBool;
     ?>
+
 
 </body>
 
