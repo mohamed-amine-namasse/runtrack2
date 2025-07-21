4168c223e0809d75791636b4d71ea05e07f11ebf
MedAmine36 <mohamed-amine.namasse@laplateforme.io>
2025-07-21 12:01:06 +0200

    edit

---
+++ jour01/job02/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    echo "<p>Hello LaPlateforme!</p>";// Ceci est un commentaire sur une seule ligne
+    echo "<p>Hello LaPlateforme!</p>";/* Ceci est un commentaire sur
+       plusieurs lignes */
+    ?>
+</body>
+
+</html>
