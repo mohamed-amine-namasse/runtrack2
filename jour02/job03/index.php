481b31f9b78cd7e64566f997de0b1ed3ce9f3a32
MedAmine36 <mohamed-amine.namasse@laplateforme.io>
2025-07-22 11:05:33 +0200

    done

--- jour02/job03/index.php
+++ jour02/job03/index.php
@@ -9,9 +9,15 @@
 
 <body>
     <?php
-    //Afficher tous les nombres de 0 à 1337 compris SAUF 26, 37, 88, 1111 et 3233
-    for( $i = 1; $i < 1337; $i++ ) {
-    if(0=<$i<20)    
+    
+    for( $i = 0; $i <= 100; $i++ ) {
+    if(0<$i && $i<20)    //si i est entre 0 et 20 le mettre en italique
+        echo "<i>$i</i>". "<br />";
+    elseif(25<$i && $i< 50 && $i!=42)  //si i est entre 25 et 50 le mettre souligné
+        echo "<u>$i</u>". "<br />";
+    elseif($i==42)  //si i égal à 42 écrire “La Plateforme_” 
+        echo "La Plateforme_" . '<br />';
+    else        // afficher normalement si différent de 42
         echo $i . '<br />';
     }
     ?>
