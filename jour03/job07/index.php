b65f9bc4c02b9deb4010a481df4d1f9e47950fda
MedAmine36 <mohamed-amine.namasse@laplateforme.io>
2025-07-23 14:34:32 +0200

    job07 terminé

--- jour03/job07/index.php
+++ jour03/job07/index.php
@@ -12,6 +12,13 @@
     $str="Certaines choses changent, et d'autres ne changeront jamais.";
     $longueur = strlen($str); // longueur de la chaine de caractère
     
+    $p_lettre=$str[0]; // on stocke la première lettre
+    for ($i= 0; $i<$longueur-1; $i++) { //boucle pour remplacer chaque lettre par celle qui la suit
+        
+        $str[$i] = $str[$i+1];
+    }
+    $new_str=substr_replace($str,$p_lettre,$longueur-1,0);
+    echo $new_str;
     
     ?>
 </body>
