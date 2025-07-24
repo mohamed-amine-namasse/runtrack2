bfe0b1bc44e6989d554f26cb8b034f089d48f460
MedAmine36 <mohamed-amine.namasse@laplateforme.io>
2025-07-24 10:17:08 +0200

    job06

---
+++ jour04/job06/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <form action="index.php" method="get">
+        <label>nombre:</label><br>
+        <input type="number" name="nombre"><br>
+        <input type="submit" value="Envoyer">
+
+    </form>
+
+
+    <?php
+    if( $_GET["nombre"] % 2 == 0 ){
+       echo "Nombre pair";
+    }
+    else {
+        echo "Nombre impair";
+    }
+    
+   
+    
+    
+    
+    ?>
+</body>
