4c284d43b791eb4a26bb9550709c156de1a0b771
MedAmine36 <mohamed-amine.namasse@laplateforme.io>
2025-07-24 09:56:55 +0200

    job05 terminé

---
+++ jour04/job05/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <form action="index.php" method="post">
+        <label>username:</label><br>
+        <input type="text" name="username"><br>
+        <label>password:</label><br>
+        <input type="password" name="password"><br>
+        <input type="submit" value="Envoyer">
+
+    </form>
+
+
+    <?php
+    if( $_POST["username"]=="John" && $_POST["password"]=="Rambo" ){
+       echo "C’est pas ma guerre";
+    }
+    else{
+        echo "Votre pire cauchemar";
+    }
+    
+   
+    
+    
+    
+    ?>
+</body>
+
+
+</html>
