dae6048c36c8b98327baaf2c69e6cfe2172d5eb4
MedAmine36 <mohamed-amine.namasse@laplateforme.io>
2025-07-24 09:35:56 +0200

    job03 terminé

---
+++ jour04/job03/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <form action="index.php" method="post">
+        <label>prénom:</label><br>
+        <input type="text" name="prenom"><br>
+        <label>nom:</label><br>
+        <input type="text" name="nom"><br>
+        <input type="submit" value="Envoyer">
+
+    </form>
+
+
+    <?php
+    $nombre_POST = count($_POST);
+    echo "le nombre de d'argument POST envoyé est:" .$nombre_POST;
+    ?>
+</body>
+
+
+</html>
