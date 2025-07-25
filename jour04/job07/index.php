46b46bbe0a59d69b50b980a7fd0668adb5392d14
MedAmine36 <mohamed-amine.namasse@laplateforme.io>
2025-07-25 15:50:08 +0200

    modif job07

--- jour04/job07/index.php
+++ jour04/job07/index.php
@@ -20,41 +20,49 @@
 
     <?php
 
-      
-        
-    
-
     
     if (isset($_GET["largeur"])&& isset($_GET["hauteur"])) {
-    // Définir les dimensions du rectangle de la maison
-    $largeur=$_GET["largeur"];
-    $hauteur = $_GET["hauteur"]+2;
-
-
-
+    
 
     // Définir les dimensions du triangle
     $hauteur_triangle = $_GET["hauteur"];
+    $largeur_triangle = $_GET["largeur"];
+
+    // Définir les dimensions du rectangle de la maison
     
     
+    $hauteur = $hauteur_triangle+2;
+
+   
+
+
+
+
+
+
     // Boucle pour chaque ligne du triangle
     for ($i = 1; $i <= $hauteur_triangle; $i++) {
-        // Afficher les espaces pour centrer le triangle 
-        for ($j = 1; $j <= $hauteur_triangle - $i; $j++) {
+        // Calcul du nombre de caractères à afficher sur la ligne
+        $nb_chars = ($largeur_triangle - 1) / ($hauteur_triangle - 1) * ($i - 1) + 1;
+        $nb_chars = intval(round($nb_chars));
+        // Espaces pour centrer
+        $espaces = intval(($largeur_triangle - $nb_chars) / 2);
+        for ($j = 0; $j < $espaces; $j++) {
             echo "&nbsp;&nbsp;";
         }
          // Affichage du toit
-        for ($k = 1; $k <= 2 * $i - 1; $k++) {
+        for ($k = 1; $k <= $nb_chars; $k++) {
             
             if ($i == 1) {
                 // Sommet du triangle :
                 echo "/\\";
             }
+            
             elseif ($k == 1) {//pour les bords de droite du toit
                 echo "/";
             } 
-            elseif ($k == 2 * $i - 1) {//pour les bords de gauche du toit
-                echo "&nbsp;\\";
+            elseif ($k == $nb_chars) {//pour les bords de gauche du toit
+                echo "\\";
             
             }
             elseif ($i == $hauteur_triangle) {echo "&nbsp;&nbsp";}
@@ -67,13 +75,13 @@
     }
     //boucle pour le rectangle 
     for( $i = 1; $i <=$hauteur; $i++ ){
-        for( $j = 1; $j <=$largeur;$j++){
+        for( $j = 1; $j <=$largeur_triangle;$j++){
             //mettre une étoile si l'indice de ligne est égal à la première ou dernière ligne
             if( $i==1||$i==$hauteur ){
                 echo "_";
             }
             //mettre une étoile si l'indice de colonne est égal à la première colonne ou la dernière
-            elseif($j==1 || $j==$largeur){
+            elseif($j==1 || $j==$largeur_triangle){
                 echo "|";
             }
             //mettre des espaces au milieu
@@ -84,6 +92,7 @@
         echo "<br/>";
     }
     }
+
     
     ?>
 </body>
