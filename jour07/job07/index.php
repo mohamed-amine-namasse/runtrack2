7ecbb10a0a183cf7c395ff7657f47f9fe909fcb7
MedAmine36 <mohamed-amine.namasse@laplateforme.io>
2025-07-28 13:40:28 +0200

    edit indent

--- jour07/job07/index.php
+++ jour07/job07/index.php
@@ -24,70 +24,70 @@
     </form>
 
     <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $str = $_POST['str'];
-    $fonction = $_POST['fonction'];
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $str = $_POST['str'];
+        $fonction = $_POST['fonction'];
 
-    // Fonction pour mettre en gras les mots commençant par une majuscule
-    function gras($text) {
-        return preg_replace_callback('/\b\w+\b/', function($matches) {
-            $word = $matches[0];
-            if (ctype_upper($word[0])) {
-                return "<b>$word</b>";
-            } else {
-                return $word;
-            }
-        }, $text);
-    }
-
-    // Fonction pour décaler chaque caractère (chiffrement de César)
-    function cesar($text, $decalage = 2) {
-        $result = '';
-        for ($i = 0; $i < strlen($text); $i++) {
-            $char = $text[$i];
-            if (ctype_alpha($char)) {
-                $base = ctype_upper($char) ? ord('A') : ord('a');
-                $offset = (ord($char) - $base + $decalage) % 26;
-                $result .= chr($base + $offset);
-            } else {
-                $result .= $char;
-            }
+        // Fonction pour mettre en gras les mots commençant par une majuscule
+        function gras($text) {
+            return preg_replace_callback('/\b\w+\b/', function($matches) {
+                $word = $matches[0];
+                if (ctype_upper($word[0])) {
+                    return "<b>$word</b>";
+                } else {
+                    return $word;
+                }
+            }, $text);
         }
-        return $result;
-    }
 
-    // Fonction pour ajouter un "_" aux mots finissant par "me"
-    function plateforme($text) {
-        return preg_replace_callback('/\b\w+\b/', function($matches) {
-            $word = $matches[0];
-            if (substr($word, -2) === 'me') {
-                return $word . '_';
-            } else {
-                return $word;
+        // Fonction pour décaler chaque caractère (chiffrement de César)
+        function cesar($text, $decalage = 2) {
+            $result = '';
+            for ($i = 0; $i < strlen($text); $i++) {
+                $char = $text[$i];
+                if (ctype_alpha($char)) {
+                    $base = ctype_upper($char) ? ord('A') : ord('a');
+                    $offset = (ord($char) - $base + $decalage) % 26;
+                    $result .= chr($base + $offset);
+                } else {
+                    $result .= $char;
+                }
             }
-        }, $text);
+            return $result;
+        }
+
+        // Fonction pour ajouter un "_" aux mots finissant par "me"
+        function plateforme($text) {
+            return preg_replace_callback('/\b\w+\b/', function($matches) {
+                $word = $matches[0];
+                if (substr($word, -2) === 'me') {
+                    return $word . '_';
+                } else {
+                    return $word;
+                }
+            }, $text);
+        }
+
+        // Appliquer la transformation selon le choix
+        $result = '';
+
+        switch ($fonction) {
+            case 'gras':
+                $result = gras($str);
+                break;
+            case 'cesar':
+                $result = cesar($str, 2);
+                break;
+            case 'plateforme':
+                $result = plateforme($str);
+                break;
+            default:
+                $result = $str;
+        }
+
+        echo "<h2>Résultat :</h2>";
+        echo "<p>$result</p>";
     }
-
-    // Appliquer la transformation selon le choix
-    $result = '';
-
-    switch ($fonction) {
-        case 'gras':
-            $result = gras($str);
-            break;
-        case 'cesar':
-            $result = cesar($str, 2);
-            break;
-        case 'plateforme':
-            $result = plateforme($str);
-            break;
-        default:
-            $result = $str;
-    }
-
-    echo "<h2>Résultat :</h2>";
-    echo "<p>$result</p>";
-}
 ?>
 
 
