5ac649139a75babacab6e2ebe48a902c9cdf6e94
MedAmine36 <mohamed-amine.namasse@laplateforme.io>
2025-07-28 16:14:54 +0200

    edit maison

--- jour04/job07/index.php
+++ jour04/job07/index.php
@@ -28,72 +28,70 @@
     $hauteur_triangle = $_GET["hauteur"];
     $largeur_triangle = $_GET["largeur"];
 
-    // Définir les dimensions du rectangle de la maison
     
     
-    $hauteur = $hauteur_triangle+2;
 
-   
+    // S'assurer que la largeur est impaire
+    if ($largeur_triangle % 2 == 0) {
+        $largeur_triangle++; // arrondi à l'impair supérieur
+    }
 
+    // Calcul de la hauteur à partir de la largeur
+    $hauteur_triangle = intval(($largeur_triangle + 1) / 2);
 
+    // Boucle pour chaque ligne du triangle (défini par la largeur)
+    for ($i = 0; $i < $hauteur_triangle; $i++) {
+        // Nombre d'espaces à gauche pour centrer
+        $espaces = $hauteur_triangle - $i - 1;
+        echo str_repeat("&nbsp;&nbsp;", $espaces);
 
+        // Largeur de la ligne courante
+        $largeur_ligne = 2 * $i + 1;
 
-
-
-    // Boucle pour chaque ligne du triangle
-    for ($i = 1; $i <= $hauteur_triangle; $i++) {
-        // Calcul du nombre de caractères à afficher sur la ligne
-        $nb_chars = ($largeur_triangle - 1) / ($hauteur_triangle - 1) * ($i - 1) + 1;
-        $nb_chars = intval(round($nb_chars));
-        // Espaces pour centrer
-        $espaces = intval(($largeur_triangle - $nb_chars) / 2);
-        for ($j = 0; $j < $espaces; $j++) {
-            echo "&nbsp;&nbsp;";
-        }
-         // Affichage du toit
-        for ($k = 1; $k <= $nb_chars; $k++) {
-            
-            if ($i == 1) {
-                // Sommet du triangle :
-                echo "/\\";
-            }
-            
-            elseif ($k == 1) {//pour les bords de droite du toit
-                echo "/";
-            } 
-            elseif ($k == $nb_chars) {//pour les bords de gauche du toit
-                echo "\\";
-            
-            }
-            elseif ($i == $hauteur_triangle) {echo "&nbsp;&nbsp";}
-            else {   // on met des traits entre les 2 bords
-                echo "_";
+        for ($j = 0; $j < $largeur_ligne; $j++) {
+            if ($j == 0) {
+                echo "/"; // bord gauche
+            } elseif ($j == $largeur_ligne - 1) {
+                echo "\\"; // bord droit
+            } elseif ($i == $hauteur_triangle - 1) {
+                echo "_"; // base
+            } else {
+                echo "&nbsp;&nbsp;"; // intérieur
             }
         }
-        // Aller à la ligne suivante
         echo "<br/>";
     }
+
+
+
+
+
+
+
 
     //boucle pour le rectangle 
-    for( $i = 1; $i <=$hauteur; $i++ ){
-        for( $j = 1; $j <=$largeur_triangle;$j++){
-            //mettre un trait horizontal si l'indice de ligne est égal à la première ou dernière ligne
-            if( $i==1||$i==$hauteur ){
-                echo "_";
-            }
-            //mettre un trait vertical si l'indice de colonne est égal à la première colonne ou la dernière
-            elseif($j==1 || $j==$largeur_triangle){
-                echo "|";
-            }
-            //mettre des espaces au milieu
-            else{
-                echo "&nbsp;&nbsp;";
-            }
-        }
-        echo "<br/>";
-    }
-    }
+    
+    for( $i = 1; $i <= $hauteur_triangle-1; $i++ ){
+   
 
+    
+    for( $j = 1; $j <= $largeur_triangle; $j++ ){
+        //mettre un trait vertical si l'indice de colonne est égal à la première colonne ou la dernière
+        if($j==1 || $j==$largeur_triangle){
+            echo "|";
+        }
+        elseif($i == $hauteur_triangle-1){
+            //le trait de la base du rectangle
+            echo "_";
+        }
+        //mettre des espaces au milieu
+        else{
+            echo "&nbsp;&nbsp;";
+        }
+    }
+    echo "<br/>";
+    }
+    }
     
     ?>
 </body>
