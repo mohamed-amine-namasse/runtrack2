7993a84e620b1fd51da7bca0ae3d2f67d5dcbad8
MedAmine36 <mohamed-amine.namasse@laplateforme.io>
2025-07-29 09:41:28 +0200

    job01 terminé

---
+++ jour08/job01/index.php
@@ -0,0 +1,33 @@
+<?php
+// Start the session
+session_start();
+
+// Réinitialisation si le bouton reset est cliqué
+if (isset($_POST['reset'])) {
+    $_SESSION['nbvisites'] = 0;
+}
+
+// Incrémentation du compteur
+if (!isset($_SESSION['nbvisites'])) {
+    $_SESSION['nbvisites'] = 1;
+} else {
+    $_SESSION['nbvisites']++;
+}
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>Nombre de visites : <?php echo $_SESSION['nbvisites']; ?></p>
+    <form method="post">
+        <button type="submit" name="reset">reset</button>
+    </form>
+</body>
+
+</html>
