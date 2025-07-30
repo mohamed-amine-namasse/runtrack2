8ed7d6186cae96f38cf52a76e26705f634e92f0a
MedAmine36 <mohamed-amine.namasse@laplateforme.io>
2025-07-30 14:11:35 +0200

    job16 terminé

--- jour09/job16/job16.sql
+++ jour09/job16/job16.sql
@@ -1,3 +1,3 @@
 SELECT etages.nom ,salles.nom AS "Biggest Room", salles.capacite
 FROM  etages ,salles
-WHERE salles.capacite=(SELECT MAX(capacite) FROM salle ) AND etages.id=salle.id_etage;
+WHERE salles.capacite=(SELECT MAX(capacite) FROM salles ) AND etages.id=salles.id_etage;
