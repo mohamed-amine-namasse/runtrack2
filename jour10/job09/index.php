ef940f4d18928f6c156971879d2b6320c01c0236
MedAmine36 <mohamed-amine.namasse@laplateforme.io>
2025-07-31 13:38:48 +0200

    job10 terminé

---
+++ jour10/job09/index.php
@@ -0,0 +1,52 @@
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+    <?php
+    // on établit la connexion avec la base de donnée jour09
+    $connexion = mysqli_connect('localhost', 'root');
+    mysqli_select_db($connexion, 'jour09'); 
+    if($connexion){
+        echo "tu es connecté!";
+    }
+    else{echo "Echec de connexion";}
+    //on fait une requete SQL pour récuperer l’ensemble des informations des salles en les triant par capacité décroissante.
+
+    $command= "SELECT * FROM salles ORDER BY capacite DESC";
+    $result = mysqli_query($connexion, $command);
+    
+
+
+    ?>
+    <br><br><br>
+    <table>
+
+        <tr>
+            <?php        //on recupère le header de notre table 
+            $fields = mysqli_fetch_fields($result);
+            foreach ($fields as $field) {
+            echo"<th>".htmlspecialchars($field->name)."</th>";}
+            ?>
+        </tr>
+        <?php    //on recupère le body de notre table
+            while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr>";
+            foreach ($fields as $field) {
+            $fieldName = $field->name;
+            echo "<td>" . htmlspecialchars($row[$fieldName]) . "</td>";
+            }
+            echo"</tr>";
+            }
+            
+            ?>
+
+    </table>
+</body>
+
+</html>
